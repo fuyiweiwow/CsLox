48fa3751f0127bfb8bfd3e9fd869a1747eab1c0b
富祎玮 <yiwei.fu@yisuworld.com>
2023-12-29 18:01:07 +0800

    update CsLox

---
+++ CsLox.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace CsLox
+{
+    public class CsLox
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length > 1)
+            {
+                Console.WriteLine("Usage: CsLox [script]");
+
+            }
+
+            if(args.Length == 1)
+            {
+                //run File
+                RunFile(args[0]);
+                return;
+            }
+
+            //run prompt
+
+
+        }
+
+        private static void RunFile(string path)
+        {
+            var bytes = 
+        }
+
+
+    }
+}
